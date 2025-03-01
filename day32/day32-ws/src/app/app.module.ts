76c7bc4ea9a9adaf3000484db9aee2cfffc7c79f
ssjh96 <ssjh96@outlook.com>
2025-03-01 23:51:01 +0800

    added components for day32-ws

--- day32/day32-ws/src/app/app.module.ts
+++ day32/day32-ws/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoComponent } from './component/todo/todo.component';
+import { TaskComponent } from './component/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
---
+++ day32/day32-ws/src/app/component/task/task.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: false,
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {
+
+}
